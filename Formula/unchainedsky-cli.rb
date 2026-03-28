9f21f775aada75e277f95b20dbd2c062255eb5fd
protostatis <121765396+protostatis@users.noreply.github.com>
2026-03-28 13:15:36 -0500

    Fix formula: use PyPI URL and correct websockets sha256

--- Formula/unchainedsky-cli.rb
+++ Formula/unchainedsky-cli.rb
@@ -3,16 +3,15 @@ class UnchainedskyCli < Formula
 
   desc "Browser automation CLI over local Chrome CDP"
   homepage "https://github.com/protostatis/unchainedsky-cli"
-  url "https://github.com/protostatis/unchainedsky-cli/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  head "https://github.com/protostatis/unchainedsky-cli.git", branch: "main"
+  url "https://files.pythonhosted.org/packages/e2/1c/7b97286d6345dd35b91dc3900b6e69da9550fbedf7f8323b7a1af4bd3337/unchainedsky_cli-0.1.1.tar.gz"
+  sha256 "740b2de6fa19070ec0c9459126f59957f2ac735baf9b3a742da9bce2fe656128"
   license "MIT"
 
   depends_on "python@3.13"
 
   resource "websockets" do
     url "https://files.pythonhosted.org/packages/source/w/websockets/websockets-16.0.tar.gz"
-    sha256 "a8429ae538944e1e5ebf03da3c0951c7e93b17e50b703a405b38c0e688c18330"
+    sha256 "5f6261a5e56e8d5c42a4497b364ea24d94d9563e8fbd44e78ac40879c60179b5"
   end
 
   def install
